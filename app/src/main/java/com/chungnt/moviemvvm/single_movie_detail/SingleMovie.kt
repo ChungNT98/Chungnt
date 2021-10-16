8c6f503ba0eb8b7842581b065127043818a7678e
ChungNT98 <91796314+ChungNT98@users.noreply.github.com>
2021-10-17 00:40:04 +0700

    init

---
+++ app/src/main/java/com/chungnt/moviemvvm/single_movie_detail/SingleMovie.kt
@@ -0,0 +1,12 @@
+ package com.chungnt.moviemvvm.single_movie_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chungnt.moviemvvm.R
+
+ class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
